2d5b052863a88cc7acf0cac8fe4fc23a11011b50
tusharhamed <tushar@nogorsolutions.com>
2021-08-15 17:12:13 +0600

    Complete project

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-container>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Home</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ms-auto">
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-container class="mt-5">
+      <b-card class="shadow-sm border-0">
+        <h2 class="text-success"><b-icon-list-check></b-icon-list-check> Data Collection</h2>
+        <b-card-body>
+          <Form :formData="data" @addmore="addNewData" @remove="removeField" />
+        </b-card-body>
+      </b-card>
+    </b-container>
+
+  </div>
+</template>
+
+<script>
+
+import Form from "./components/Form";
+export default {
+  name: 'App',
+  components: {Form},
+  data(){
+    return{
+      data:[
+        {
+          name:'',
+          phone:'',
+          address:''
+        }
+      ]
+    }
+  },
+  methods:{
+    addNewData(){
+      this.data.push({
+        name:'',
+        phone:'',
+        address:''
+      })
+    },
+    removeField(index){
+      this.data.splice(index,1)
+    }
+  }
+}
+</script>
+
---
+++ src/components/Form.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="form">
+        <b-form>
+            <b-row v-for="(field,index) in formData" :key="index" class="mb-3">
+                <b-col lg="3">
+                    <b-form-group>
+                        <b-form-input placeholder="Name" v-model="field.name" />
+                    </b-form-group>
+                </b-col>
+                <b-col lg="3">
+                    <b-form-group>
+                        <b-form-input placeholder="Phone" type="number" v-model="field.phone" />
+                    </b-form-group>
+                </b-col>
+                <b-col lg="3">
+                    <b-form-group>
+                        <b-form-input placeholder="Address"  v-model="field.address" />
+                    </b-form-group>
+                </b-col>
+                <b-col lg="2" class="text-center d-flex justify-content-around align-items-center">
+                    <b-btn v-if="index > 0" @click="$emit('remove',index)" class="btn-sm" variant="danger"><b-icon-x /></b-btn>
+                    <b-btn v-if="index === formData.length -1" @click="$emit('addmore')" class="btn-sm" variant="success"><b-icon-plus /></b-btn>
+                </b-col>
+            </b-row>
+        </b-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['formData'],
+        name: "Form"
+    }
+</script>
+
+<style scoped>
+
+</style>
